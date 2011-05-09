9897ba7d82dba6fa94e1530a0870a37f0ecc7c5e
Brent Miller <foliosus@foliosus.com>
2011-05-08 23:50:25 -0700

    Show flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def show_flash_messages
+    output = ''
+    [:notice, :info, :warning, :error].each do |level|
+      Rails.logger.warn(level.to_s + ": " + flash[level].inspect)
+      if flash[level]
+        output += content_tag(:div, :class => level) do
+          content_tag :p, h(flash[level])
+        end
+      end
+    end
+    if output.blank?
+      nil
+    else
+      content_tag(:div, output.html_safe, :id => 'communication')
+    end
+  end
 end
